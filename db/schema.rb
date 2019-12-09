6e9f3f87119de5f7971713c246cff97adbceb273
devseyoung <devseyoung@gmail.com>
2019-12-09 12:23:50 +1000

    add comment function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_010901) do
+ActiveRecord::Schema.define(version: 2019_12_07_005117) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
